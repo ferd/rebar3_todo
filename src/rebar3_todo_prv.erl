328c8d4fc5f58f963262f3c5ca4951fadbaec672
kecote <kecote@genetec.com>
2018-04-04 14:25:08 -0400

    Remove extraneous line

--- src/rebar3_todo_prv.erl
+++ src/rebar3_todo_prv.erl
@@ -58,7 +58,6 @@ check_todo_app(App) ->
 
 find_source_files(Path) ->
     filelib:fold_files(Path, ".*\.erl", true, fun(File, Acc) -> [File|Acc] end, []).
-    [filename:join(Path, Mod) || Mod <- filelib:wildcard("*.erl", Path)].
 
 check_todo_mod(ModPath, Matches) ->
     {ok, Bin} = file:read_file(ModPath),
